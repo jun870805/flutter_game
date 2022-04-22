d0baf1c98ae7d3d9201f6adccad142d018ec1aff
jun870805 <jun870805@gmail.com>
2022-04-23 00:43:29 +0800

    移除firebase

--- lib/game/flutter_game.dart
+++ lib/game/flutter_game.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/input.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:flame/flame.dart';
